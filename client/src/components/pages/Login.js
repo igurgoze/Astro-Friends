e41dd4dbf158bbf4f8b65fc1e3e868762e64b5f1
lsegura06 <leosegura006@gmail.com>
2023-03-15 07:19:21 -0700

    styled navbar, login, and register

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
 import '../../styles/style.css';
 
@@ -11,34 +11,44 @@ export default function LoginForm() {
   };
 
   return (
-    <div className='login-background' style={{color: 'white'}}>
-    <div className="d-flex justify-content-center">
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter username"
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-          />
-        </Form.Group>
+    <body className="login-background app-container" style={{ fontFamily: "'Press Start 2P', cursive" }}>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-6 offset-lg-3">
+            <h1 className="text-center fade-in-out mb-4" style={{ color: "white" }}>Enter Your Ship Here</h1>
+            <Form onSubmit={handleSubmit}>
+              <Form.Group controlId="formUsername">
+                <Form.Label style={{ color: "white", fontFamily: "'Press Start 2P', cursive" }}>Username</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Enter username or email..."
+                  value={username}
+                  onChange={(event) => setUsername(event.target.value)}
+                  style={{ backgroundColor: "transparent", color: "white", border: "1px solid white", outline: "none", fontFamily: "'Press Start 2P', cursive" }}
+                />
+              </Form.Group>
 
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control
-            type="password"
-            placeholder="Password"
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </Form.Group>
+              <Form.Group controlId="formPassword">
+                <Form.Label style={{ color: "white", fontFamily: "'Press Start 2P', cursive" }}>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  style={{ backgroundColor: "transparent", color: "white", border: "1px solid white", outline: "none", fontFamily: "'Press Start 2P', cursive" }}
+                />
+              </Form.Group>
 
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form>
-    </div>
-    </div>
+              <div className="d-flex justify-content-between align-items-center">
+                <Button variant="primary" type="submit" style={{ backgroundColor: "transparent", color: "white", border: "1px solid white", fontFamily: "'Press Start 2P', cursive" }}>
+                  Login
+                </Button>
+                
+              </div>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </body>
   );
 }
